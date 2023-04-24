e73f1ce68367263d121f32ff94b4f98563a3256e
Edgy-Floors <63993823+Edgy-Floors@users.noreply.github.com>
2023-04-24 11:00:14 -0500

    Deleted some deprecated code

--- CIS 450 Final Project/Assets/Scripts/MainMenu.cs
+++ CIS 450 Final Project/Assets/Scripts/MainMenu.cs
@@ -2,15 +2,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    [Tooltip("The level name must be typed exactly as it is saved as.")]
-    public string firstLevel;
-    public string secondLevel;
-
-    public void StartGame()
-    {
-        
-    }
-
     public void LoadFirstLevel()
     {
         SceneManager.LoadScene("Level 1");
